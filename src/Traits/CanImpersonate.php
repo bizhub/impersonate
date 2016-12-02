57559716d300c6981d2099c90636892eb26633ba
devonblzx <devon.b@virtualcomplete.com>
2016-12-01 19:51:18 -0500

    Store impersonator id and retrieve that user model using impersonator()

--- src/Traits/CanImpersonate.php
+++ src/Traits/CanImpersonate.php
@@ -2,12 +2,21 @@
 
 namespace Bizhub\Impersonate\Traits;
 
+use Exception;
 use Session;
+use Auth;
 
 trait CanImpersonate
 {
 	public function impersonate()
     {
+        if (!$this->isImpersonating()) {
+            throw new Exception('You must stop impersonating before impersonating another user.');
+        }
+        if (!Auth::check()) {
+            throw new Exception('You must be logged in before impersonating another user.');
+        }
+        Session::put('impersonator', Auth::id());
         Session::put('impersonate', $this->id);
     }
 
@@ -19,5 +28,12 @@ trait CanImpersonate
     public function isImpersonating()
     {
         return Session::has('impersonate');
+    }
+
+    public function impersonator() {
+        if ($this->isImpersonating()) {
+            return self::find(Session::get('impersonator'));
+        }
+        return null;
     }
 }
